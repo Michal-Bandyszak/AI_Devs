6bc839e5d7dac3bed15273767af954cca1c3d9ae
Michal-Bandyszak <michalbandyszak@gmail.com>
2024-04-01 21:43:11 +0200

    Finished whisper, added transcribe function to ai

--- 07-whisper/07.js
+++ 07-whisper/07.js
@@ -1,10 +1,16 @@
 import { getTask, sendAnswer } from "../api/api_connection.js";
-import { chatWithAi } from "../api/ai.js";
+import { transcribe } from "../api/ai.js";
 
 const taskName = "whisper"
 
 const main = async function(){
-    const task = await getTask(taskName);
-    console.log(task)
+    try {
+        const task = await getTask(taskName);
+        const answer = await transcribe()
+        await sendAnswer({"answer": answer})
+    }
+    catch (error) {
+        console.log("An error accured", error)
+    }
 }();
 

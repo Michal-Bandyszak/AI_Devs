60c15f3eecce07f84f02ab3ed507a60e1105fc87
Michal-Bandyszak <michalbandyszak@gmail.com>
2024-04-03 14:42:37 +0200

    Cleared code of scrapper

--- 10-scraper/10.js
+++ 10-scraper/10.js
@@ -6,10 +6,3 @@ const taskName = "scraper"
 const { question, input: url } = await getTask(taskName);
 const { choices: [{ message: { content: answer } }] } = await chatWithAi(`Base only on the knowledge from article ${url}. Answers longer than 200 chars are forbidden.`, question, "gpt-3.5-turbo");
 await sendAnswer({ answer });
-
-// const task = await getTask(taskName)
-// const question = task.question
-// const url = task.input
-
-// const aiResponse = await chatWithAi(`Base only on the knowledge from article ${url}. Answers longer than 200 chars are forbidden.`, question, "gpt-3.5-turbo" )
-// await sendAnswer({"answer": aiResponse.choices[0].message.content })
